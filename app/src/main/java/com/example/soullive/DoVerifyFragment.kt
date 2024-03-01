1bab375f1586008e00fdcd069b2b39d0d15985ce
jeong724 <oliveyou724@gmail.com>
2024-03-01 15:18:53 +0900

    [feat] 검증하기 탭레이아웃 연결

---
+++ app/src/main/java/com/example/soullive/DoVerifyFragment.kt
@@ -0,0 +1,27 @@
+package com.example.soullive
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.soullive.databinding.FragmentDoverifyBinding
+
+class DoVerifyFragment : Fragment() {
+
+    lateinit var binding: FragmentDoverifyBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentDoverifyBinding.inflate(inflater, container, false)
+
+        binding.writeIb.setOnClickListener {
+
+        }
+
+        return binding.root
+    }
+}
