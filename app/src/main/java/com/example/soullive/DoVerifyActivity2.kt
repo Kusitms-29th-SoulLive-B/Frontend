7e81cbecc3d9a9072ea73b97a0e8a5eadc16308f
jeong724 <oliveyou724@gmail.com>
2024-03-01 21:54:41 +0900

    [add] 가이드 레이아웃

--- app/src/main/java/com/example/soullive/DoVerifyActivity2.kt
+++ app/src/main/java/com/example/soullive/DoVerifyActivity2.kt
@@ -2,6 +2,8 @@ package com.example.soullive
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import com.example.soullive.databinding.ActivityDoverify1Binding
 import com.example.soullive.databinding.ActivityDoverify2Binding
@@ -20,5 +22,30 @@ class DoVerifyActivity2 : AppCompatActivity() {
             finish()
         }
 
+        binding.detailEt.addTextChangedListener(textWatcher)
+
+        binding.nextVerify2Btn.setOnClickListener {
+
+        }
+
+
+    }
+
+    private val textWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            // 이메일이 입력되었을 때
+            if (binding.detailEt.text.isNotEmpty()) {
+                // 버튼의 배경 변경
+                binding.nextVerify2Btn.setBackgroundResource(R.drawable.main_btn)
+                binding.nextVerify2Btn.isEnabled = true // 버튼 활성화
+            } else {
+                binding.nextVerify2Btn.setBackgroundResource(R.drawable.gray_btn)
+                binding.nextVerify2Btn.isEnabled = false // 버튼 비활성화
+            }
+        }
+
+        override fun afterTextChanged(s: Editable?) {}
     }
 }
