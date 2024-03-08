2da2554ef4955fca7168a6edd2a3e848b6b097c4
jeong724 <oliveyou724@gmail.com>
2024-03-09 01:02:11 +0900

    [add] rfp파일 검증

---
+++ app/src/main/java/com/example/soullive/verify/ScriptVerifyActivity.kt
@@ -0,0 +1,12 @@
+package com.example.soullive.verify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.soullive.R
+
+class ScriptVerifyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_script_verify)
+    }
+}
