1d9db98a9998110c134815f8dc3b92b3397276b8
jeong724 <oliveyou724@gmail.com>
2024-03-09 01:04:33 +0900

    [feat] rfp파일 검증

--- app/src/main/java/com/example/soullive/verify/ScriptVerifyActivity.kt
+++ app/src/main/java/com/example/soullive/verify/ScriptVerifyActivity.kt
@@ -3,10 +3,28 @@ package com.example.soullive.verify
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.soullive.R
+import com.example.soullive.databinding.ActivityDoverify9Binding
+import com.example.soullive.databinding.ActivityScriptVerifyBinding
 
 class ScriptVerifyActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityScriptVerifyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_script_verify)
+
+        binding = ActivityScriptVerifyBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.previous2Ib.setOnClickListener {
+            finish()
+        }
+
+        binding.close2Btn.setOnClickListener {
+            finish()
+        }
+
+
+
     }
 }
